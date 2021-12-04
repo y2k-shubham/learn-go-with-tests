37d346f26e15d475baff4434aa0a9d36e9692fdc
y2k-shubham <y2k.shubhamgupta@gmail.com>
2021-12-05 00:45:07 +0530

    [refactor][unittest] (p1_hello_world) Refactor hello test

--- fundamentals/p1_hello_world/main/hello_test.go
+++ fundamentals/p1_hello_world/main/hello_test.go
@@ -3,10 +3,12 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	var got string = Hello("Chris")
-	var want string = "Hello Chris!"
+	t.Run("greeting Hello to people", func(t *testing.T) {
+		var got string = Hello("Chris")
+		var want string = "Hello Chris!"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
