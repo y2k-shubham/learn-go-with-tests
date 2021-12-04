ba04263e78851e1347a1bd188a5b99e986fcf94c
y2k-shubham <y2k.shubhamgupta@gmail.com>
2021-12-05 00:39:21 +0530

    [dev] (p1_hello_world) Take name as input

--- fundamentals/p1_hello_world/main/hello_test.go
+++ fundamentals/p1_hello_world/main/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	var got string = Hello()
-	var want string = "Hello World!"
+	var got string = Hello("Chris")
+	var want string = "Hello Chris!"
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
