18597129c3a26b223b8a8137106ba5a0ef35b9ba
y2k-shubham <y2k.shubhamgupta@gmail.com>
2021-12-11 21:57:22 +0530

    [refactor-unittest] (p4_arrays_n_slices) Refactor test

--- fundamentals/p4_arrays_n_slices/sum_test.go
+++ fundamentals/p4_arrays_n_slices/sum_test.go
@@ -2,13 +2,21 @@ package p4_arrays_n_slices
 
 import "testing"
 
-func TestSum(t *testing.T) {
-	arr := []int{8, -1, 3, 7, -4, -2, 9}
-
-	got := Sum(arr)
-	want := 20
+func assertHelper(t testing.TB, got, want int, slc []int) {
+	t.Helper()
 
 	if got != want {
-		t.Errorf("got %d, want %d, given %v", got, want, arr)
+		t.Errorf("got %d, want %d, given %v", got, want, slc)
 	}
+}
+
+func TestSum(t *testing.T) {
+	t.Run("Slice (array of any len)", func(t *testing.T) {
+		arr := []int{8, -1, 3, 7, -4, -2, 9}
+
+		got := Sum(arr)
+		want := 20
+
+		assertHelper(t, got, want, arr)
+	})
 }
