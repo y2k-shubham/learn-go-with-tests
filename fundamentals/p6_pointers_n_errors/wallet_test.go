f9cddd7c015c9115d880c6daa97d11110b8c4290
y2k-shubham <y2k.shubhamgupta@gmail.com>
2021-12-13 01:03:20 +0530

    [robust] (p6_pointers_n_errors) Handle insufficient balance during withdrawls

--- fundamentals/p6_pointers_n_errors/wallet_test.go
+++ fundamentals/p6_pointers_n_errors/wallet_test.go
@@ -4,6 +4,8 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 	checkBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+
 		got := wallet.Balance()
 
 		if got != want {
@@ -20,12 +22,26 @@ func TestWallet(t *testing.T) {
 		checkBalance(t, wallet, want)
 	})
 
-	t.Run("Withdraw", func(t *testing.T) {
+	t.Run("Withdraw: normal", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
 		wallet.Withdraw(5)
 		want := Bitcoin(15)
 
 		checkBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw: insufficient balance", func(t *testing.T) {
+		initialBalance := Bitcoin(20)
+		wallet := Wallet{balance: initialBalance}
+
+		err := wallet.Withdraw(Bitcoin(100))
+		want := initialBalance
+
+		checkBalance(t, wallet, want)
+
+		if err == nil {
+			t.Errorf("wanted an error but didnt get one")
+		}
 	})
 }
