a6bffd749255e3a01702251ce0cb2bdc4d25e225
y2k-shubham <y2k.shubhamgupta@gmail.com>
2021-12-13 00:32:53 +0530

    [refactor] (p6_pointers_n_errors) Introduce Bitcoin type

--- fundamentals/p6_pointers_n_errors/wallet.go
+++ fundamentals/p6_pointers_n_errors/wallet.go
@@ -1,13 +1,15 @@
 package p6_pointers_n_errors
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(btc int) {
+func (w *Wallet) Deposit(btc Bitcoin) {
 	w.balance += btc
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
