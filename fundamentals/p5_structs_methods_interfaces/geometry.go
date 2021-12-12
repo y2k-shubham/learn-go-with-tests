c23b1e9a828c71581270ee78d6cd27af51b6bf0b
y2k-shubham <y2k.shubhamgupta@gmail.com>
2021-12-12 22:18:03 +0530

    [dev] (p5_structs_methods_interfaces) Convert fns to methods, introduce Circle struct

--- fundamentals/p5_structs_methods_interfaces/geometry.go
+++ fundamentals/p5_structs_methods_interfaces/geometry.go
@@ -1,14 +1,28 @@
 package p5_structs_methods_interfaces
 
-func Perimeter(rectangle Rectangle) float32 {
-	return 2 * (rectangle.Length + rectangle.Width)
-}
-
-func Area(rectangle Rectangle) float32 {
-	return rectangle.Length * rectangle.Width
-}
+import "math"
 
 type Rectangle struct {
 	Length float32
 	Width  float32
+}
+
+func (rectangle Rectangle) Perimeter() float32 {
+	return 2 * (rectangle.Length + rectangle.Width)
+}
+
+func (rectangle Rectangle) Area() float32 {
+	return rectangle.Length * rectangle.Width
+}
+
+type Circle struct {
+	Radius float32
+}
+
+func (circle Circle) Perimeter() float32 {
+	return 2 * math.Pi * circle.Radius
+}
+
+func (circle Circle) Area() float32 {
+	return float32(math.Pi * math.Pow(float64(circle.Radius), 2))
 }
