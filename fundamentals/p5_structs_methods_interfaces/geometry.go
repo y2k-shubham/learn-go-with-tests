2dea3fbbc6b4a9f4bb0b34ff88b9b89fff9a8492
y2k-shubham <y2k.shubhamgupta@gmail.com>
2021-12-12 21:08:37 +0530

    [modify] (p5_structs_methods_interfaces) Use Rectangle struct

--- fundamentals/p5_structs_methods_interfaces/geometry.go
+++ fundamentals/p5_structs_methods_interfaces/geometry.go
@@ -1,9 +1,14 @@
 package p5_structs_methods_interfaces
 
-func Perimeter(length, width float32) float32 {
-	return 2 * (length + width)
+func Perimeter(rectangle Rectangle) float32 {
+	return 2 * (rectangle.Length + rectangle.Width)
 }
 
-func Area(length, width float32) float32 {
-	return length * width
+func Area(rectangle Rectangle) float32 {
+	return rectangle.Length * rectangle.Width
+}
+
+type Rectangle struct {
+	Length float32
+	Width  float32
 }
